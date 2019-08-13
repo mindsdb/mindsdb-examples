8c272da03f14e1126e2ea11fe76b22f5b4eb9b06
Zoran Pandovski <zoran.pandovski@keitaro.com>
2019-08-13 12:18:31 +0200

    Train from local file

--- diabetes_example/train.py
+++ diabetes_example/train.py
@@ -5,6 +5,6 @@ mdb = mindsdb.Predictor(name='diabetes-class')
 
 # We tell the Predictor what column or key we want to learn and from what data
 mdb.learn(
-    from_data="https://raw.githubusercontent.com/MateLabs/Public-Datasets/master/Datasets/diabetes.csv", # the path to the file where we can learn from, (note: can be url)
+    from_data="dataset/diabetes-test.csv", # the path to the file where we can learn from, (note: can be url)
     to_predict='Class', # the column we want to learn to predict given all the data in the file
 )
