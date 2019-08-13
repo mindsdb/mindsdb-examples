bb665e8c418fdd590e7ac4e146cc97eb7b0baddc
Zoran Pandovski <zoran.pandovski@keitaro.com>
2019-08-13 12:03:08 +0200

    CHange predictor

--- air_quality/train.py
+++ air_quality/train.py
@@ -5,6 +5,6 @@ mdb = mindsdb.Predictor(name='AirQua')
 
 # We tell the Predictor what column or key we want to learn and from what data
 mdb.learn(
-    from_data="dataset/AirQualityUCITrain.csv", # the path to the file where we can learn from, (note: can be url)
-    to_predict='CO_GT', # the column we want to learn to predict given all the data in the file
+    from_data="dataset/AirQualityUCITest.csv", # the path to the file where we can learn from, (note: can be url)
+    to_predict='CO(GT)', # the column we want to learn to predict given all the data in the file
 )
