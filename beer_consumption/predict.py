bcc2c5e4c2f3af3fd64c5e3929e111d5919cd01e
Zoran Pandovski <zoran.pandovski@keitaro.com>
2019-08-22 11:25:33 +0200

    Minor changes

--- beer_consumption/predict.py
+++ beer_consumption/predict.py
@@ -1,5 +1,5 @@
 from mindsdb import Predictor
 
 # use the model to make predictions
-result = Predictor(name='beer_consumption').predict(when={'Temperatura_Media': '27.3'})
+result = Predictor(name='beer_consumption').predict(when={'Temperatura Media': '27.3','Final de Semana': 0, 'Precipitacao': '1.2'})
 print(result[0])
--- beer_consumption/train.py
+++ beer_consumption/train.py
@@ -5,7 +5,7 @@ from mindsdb.config import CONFIG
 
 # We tell mindsDB what we want to learn and from what data
 Predictor(name='beer_consumption').learn(
-    to_predict='Consumo_de_cerveja', # the column we want to learn to predict given all the data in the file
-    from_data="Consumo_cerveja.csv" # the path to the file where we can learn from, (note: can be url)
+    to_predict='Consumo de cerveja', # the column we want to learn to predict given all the data in the file
+    from_data="dataset/Consumo_cerveja_train.csv" # the path to the file where we can learn from, (note: can be url)
 )
 
