022f1065b766e2829eeab3571bb6a0baf9a4721e
Zoran Pandovski <zoran.pandovski@keitaro.com>
2019-10-24 00:07:06 +0200

    Add diabetes example

--- diabetes_example/predict.py
+++ diabetes_example/predict.py
@@ -1,5 +1,6 @@
 from mindsdb import Predictor
 
 # use the model to make predictions
-result = Predictor(name='diabetes-class').predict(when={'Plasma glucose concentration': 93, 'Diastolic blood pressure': 31, 'Number of times pregnant': 1})
+result = Predictor(name='diabetes-class').predict(when={'Plasma glucose concentration': 162, 'Diastolic blood pressure': 84,
+ 'Triceps skin fold thickness': 0, 'Age': 54, 'Number of times pregnant': 10})
 print(result[0])
--- diabetes_example/train.py
+++ diabetes_example/train.py
@@ -5,6 +5,6 @@ mdb = mindsdb.Predictor(name='diabetes-class')
 
 # We tell the Predictor what column or key we want to learn and from what data
 mdb.learn(
-    from_data="dataset/diabetes-test.csv", # the path to the file where we can learn from, (note: can be url)
+    from_data="dataset/diabetes-train.csv", # the path to the file where we can learn from, (note: can be url)
     to_predict='Class', # the column we want to learn to predict given all the data in the file
 )
