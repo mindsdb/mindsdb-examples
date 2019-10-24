d2b832b01f8643aa3fd8baf4ac899424e3925fdd
Zoran Pandovski <zoran.pandovski@keitaro.com>
2019-10-24 11:36:41 +0200

    Train test split data

--- prostate_cancer/train.py
+++ prostate_cancer/train.py
@@ -5,6 +5,6 @@ mdb = mindsdb.Predictor(name='pc')
 
 # We tell the Predictor what column or key we want to learn and from what data
 mdb.learn(
-    from_data="dataset/Prostate_Cancer.csv", # the path to the file where we can learn from, (note: can be url)
+    from_data="dataset/train-Prostate_Cancer.csv", # the path to the file where we can learn from, (note: can be url)
     to_predict='diagnosis_result', # the column we want to learn to predict given all the data in the file
 )
