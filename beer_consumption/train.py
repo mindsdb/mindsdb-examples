a627f79883160f90b048b4b8e3f39bedf0c02767
Zoran Pandovski <zoran.pandovski@keitaro.com>
2020-01-15 13:58:21 +0100

    Code cleanup

--- beer_consumption/train.py
+++ beer_consumption/train.py
@@ -5,6 +5,6 @@ from mindsdb import Predictor
 # We tell mindsDB what we want to learn and from what data
 Predictor(name='beer_consumption').learn(
     to_predict='Consumo de cerveja', # the column we want to learn to predict given all the data in the file
-    from_data="dataset/Consumo_cerveja_train.csv" # the path to the file where we can learn from, (note: can be url)
+    from_data="dataset/Consumo_cerveja_train.csv",# the path to the file where we can learn from, (note: can be url)
 )
 
