07c90d4d68f7b438bfe88ceb90f0f168c280ea48
Zoran Pandovski <zoran.pandovski@keitaro.com>
2020-02-26 20:38:56 +0100

    Use train dataset

--- others/restaurant_scores/train.py
+++ others/restaurant_scores/train.py
@@ -5,6 +5,6 @@ mdb = mindsdb.Predictor(name='restaurant_score')
 
 # We tell the Predictor what column or key we want to learn and from what data
 mdb.learn(
-    from_data="dataset/Restaurant_Scores_Test.csv", # the path to the file where we can learn from, (note: can be url)
+    from_data="dataset/Restaurant_Scores_Train.csv", # the path to the file where we can learn from, (note: can be url)
     to_predict='risk_category', # the column we want to learn to predict given all the data in the file
 )
