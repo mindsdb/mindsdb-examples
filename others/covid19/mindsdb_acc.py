10a1d327d5a0db0d1c3734dceadb276ec8771135
George3d6 <george@cerebralab.com>
2020-03-20 22:13:06 +0200

    updated the covid-19 dataset

--- others/covid19/mindsdb_acc.py
+++ others/covid19/mindsdb_acc.py
@@ -4,26 +4,30 @@ import pandas as pd
 from sklearn.metrics import balanced_accuracy_score, accuracy_score, r2_score
 
 def run():
+    backend = 'lightwood'
+    target = 'Deaths'
 
     mdb = mindsdb.Predictor(name='corona-data')
 
-    target = ['Confirmed','Recovered','Deaths']
     # We tell the Predictor what column or key we want to learn and from what data
     mdb.learn(
         from_data="processed_data/train.csv",
         to_predict=target
+        backend = backend
     )
 
     test_df = pd.read_csv('processed_data/test.csv')
     predictions = mdb.predict(when_data='processed_data/test.csv')
 
-    results = [str(x[target[0]]) for x in predictions]
-    real = list(map(str,list(test_df[target[0]])))
+    results = [str(x[target]) for x in predictions]
+    real = list(map(str,list(test_df[target])))
     accuracy = r2_score(real, results)
     print(accuracy)
-    
+
     return {
-        'accuracy': accuracy
+    'accuracy': accuracy
+    ,'accuracy_function': 'r2_score'
+    ,'backend': backend
     }
 
 
