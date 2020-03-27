d9ef35ecdc44fb00314de6d0c0750b8895cf10e0
surendragandham <g.surendra1472@gmail.com>
2020-03-27 22:28:15 +0530

    addressed comments

--- others/plant_pathology/mindsdb_acc.py
+++ others/plant_pathology/mindsdb_acc.py
@@ -1,23 +1,22 @@
 import mindsdb
 import csv
-from sklearn.metrics import accuracy_score
-import sys
-import pandas
+from sklearn.metrics import balanced_accuracy_score
+import pandas as pd
 
 
 def run():
-    train_file = 'train.csv'
-    test_file = 'test.csv'
+    train_file = 'data_set/train.csv'
+    test_file = 'data_set/test.csv'
 
     backend = 'lightwood'
 
     predictor = mindsdb.Predictor(name='plant_pathology')
 
-    train_df = pandas.read_csv(train_file)
+    train_df = pd.read_csv(train_file)
     predictor.learn(from_data=train_df, to_predict=['class'], disable_optional_analysis=True, use_gpu=False,
                     backend=backend, stop_training_in_x_seconds=round((3600 * 2)))
 
-    test_df = pandas.read_csv(test_file)
+    test_df = pd.read_csv(test_file)
     predictions = predictor.predict(when_data=test_df, unstable_parameters_dict={'always_use_model_prediction': True},
                                     use_gpu=False)
 
@@ -31,17 +30,16 @@ def run():
             if first:
                 first = False
             else:
-                real_class.append(row[2])
+                real_class.append(row[1])
 
-    acc = accuracy_score(real_class, predicted_class) * 100
-    print(f'Log loss accuracy of {acc}% for classes !')
+    acc = balanced_accuracy_score(real_class, predicted_class) * 100
+    print(f'Balanced accuracy of {acc}% for classes !')
 
     return {
         'accuracy': acc
-        , 'accuracy_function': 'accuracy_score'
+        , 'accuracy_function': 'balanced_accuracy_score'
         , 'backend': backend
     }
-
 
 
 # Run as main
