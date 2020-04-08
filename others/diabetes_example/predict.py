166e622eb4333d7afc4930a7b00507b54c62c54a
George3d6 <george@cerebralab.com>
2020-04-08 21:55:06 +0300

    made an example work

--- others/diabetes_example/predict.py
+++ others/diabetes_example/predict.py
@@ -1,6 +1,16 @@
 from mindsdb import Predictor
+import pandas as pd
+from sklearn.metrics import balanced_accuracy_score, accuracy_score
+import lightwood
 
+
+lightwood.config.config.CONFIG.FORCE_HELPER_MIXERS = True
 # use the model to make predictions
-result = Predictor(name='diabetes-class').predict(when={'Plasma glucose concentration': 162, 'Diastolic blood pressure': 84,
- 'Triceps skin fold thickness': 0, 'Age': 54, 'Number of times pregnant': 10})
-print(result[0])
+result = Predictor(name='diabetes-class').predict(when_data='dataset/diabetes-test.csv')
+real = pd.read_csv('dataset/diabetes-test.csv')['Class']
+
+p = [str(x['Class']) for x in result]
+print(p)
+r = [str(x) for x in real]
+print(r)
+print(accuracy_score(r,p))
--- others/diabetes_example/train.py
+++ others/diabetes_example/train.py
@@ -5,7 +5,7 @@ mdb = mindsdb.Predictor(name='diabetes-class')
 
 # We tell the Predictor what column or key we want to learn and from what data
 mdb.learn(
-    from_data="dataset/diabetes-train.csv", # the path to the file where we can learn from, (note: can be url)
-    to_predict='Class', # the column we want to learn to predict given all the data in the file
+    from_data="dataset/diabetes-train.csv",
+    to_predict='Class',
     equal_accuracy_for_all_output_categories=True
 )
