affbea408807e99ef7ee7addf7eb1db3d59727ab
Zoran Pandovski <zoran.pandovski@keitaro.com>
2020-04-13 10:44:43 +0200

    Re-structure

--- mindsb_server/server.py
+++ mindsb_server/server.py
@@ -2,50 +2,42 @@ import requests
 import pandas
 import json
 
-class MindsdbServerSDK:
 
-    def __init__(self, data):
-        self.url = data['server_url']
-        self.dataset = data['dataset']
-        self.to_predict = data['to_predict']
-
-    def create_predictor(self):
-        '''
-        Learn to predict a given variable from existing data.
-        '''
-        data = dict(
-            to_predict = self.to_predict,
-            from_data = dataset
-        )
-        response = requests.request('PUT', self.url, json=data)
-        return response
-
-    
-    def get_predictor_metadata(self):
-        '''
-        Get metadatadata for a given predictor.
-        '''
-        response = requests.request("GET", self.url)
-        return response.text
+def create_predictor(url, dataset, to_predict):
+    '''
+    Learn to predict a given variable from existing data.
+    :param url: the create predictor service url
+    :param dataset: the data to train the predictor
+    :param to_predict: feature to predict
+    :returns: status code
+    '''
+    data = dict(
+        to_predict = to_predict,
+        from_data = dataset
+    )
+    response = requests.request('PUT', url, json=data)
+    return response
 
 
-    def query_predictor(self):
-        '''
-        Send prediction queries to a specific predictor
-        '''
-        data = {
-            'when': {
-                'age': '25',
-                'sex': '0',
-                'chol': '150',
-                'thalach': '170',
-                'exang': '0',
-                'fbs': '0',
-                'thal': '3',
-            }
-        }
-        response = requests.request('POST', self.url + '/predict', json=data)
-        return response.text
+def get_predictor_metadata(url):
+    '''
+    Get metadatadata for a given predictor.
+    :param url: get predictor metadata service url
+    :returns: metadata info as json
+    '''
+    response = requests.request("GET", url)
+    return response.text
+
+
+def query_predictor(url, data):
+    '''
+    Send prediction queries to a specific predictor.
+    :param url: query predictor service url
+    :param data: the data contains the query
+    :returns: model predictions as json
+    '''
+    response = requests.request('POST', url + '/predict', json=data)
+    return response.text
 
 
 if __name__ == "__main__":
@@ -55,22 +47,29 @@ if __name__ == "__main__":
     
     # the name of the predictor
     predictor_name = 'test_predictor'
+    # endpoint url
+    url = 'http://localhost:47334/predictors/' + predictor_name
 
     # url to dataset
     dataset = 'https://raw.githubusercontent.com/mindsdb/mindsdb-examples/master/benchmarks/heart_disease/processed_data/train.csv'
     
-    data = dict(
-        server_url = 'http://localhost:47334/predictors/' + predictor_name,
-        dataset = dataset,
-        to_predict = to_predict
-    )
-    server = MindsdbServerSDK(data)
 
     # create new predictor
-    #server.create_predictor()
+    #print(create_predictor(url, dataset, to_predict))
 
     # get predictor metadata
-    print(server.get_predictor_metadata())
+    #print(get_predictor_metadata(url))
 
     # get predictions
-    #print(server.query_predictor())
+    data = {
+        'when': {
+            'age': '25',
+            'sex': '0',
+            'chol': '150',
+            'thalach': '170',
+            'exang': '0',
+            'fbs': '0',
+            'thal': '3',
+        }
+    }
+    print(query_predictor(url, data))
