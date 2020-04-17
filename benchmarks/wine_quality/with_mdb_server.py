ae770a294657b76f47891c1ae3d1232ea4d9c067
George3d6 <george@cerebralab.com>
2020-04-17 05:15:08 +0300

    perfect

--- benchmarks/wine_quality/with_mdb_server.py
+++ benchmarks/wine_quality/with_mdb_server.py
@@ -51,26 +51,26 @@ predicted_range_arr = [x['price']['confidence_interval'] for x in predictions]
 prediction_confidence_arr = [x['price']['confidence'] for x in predictions]
 
 # Looking at our predictions
-from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error
 import numpy as np
 import pandas as pd
 
-high_percentile_confidence = np.percentile(prediction_confidence_arr, 85)
-
+high_percentile_confidence = np.percentile(prediction_confidence_arr, 91)
 # The real values
 test_data = pd.read_csv(open('processed_data/test.csv', 'r'))
 real_val_arr = list(test_data['price'])
-real_val_arr = [x if str(x) != 'nan' or x <= 0 else 0.001 for x in real_val_arr]
-exact_prediction_arr_no_nan  = [x if str(x) != 'nan' and x is not None and x <= 0 else 0.001 for x in real_val_arr]
+ignore_indexes_arr = [index if str(real_val_arr[index]) == 'nan' else -1 for index in range(len(real_val_arr))]
 # A good score for comparing the accuracy value between two predictors... but not much meaning unless you are a statistician and know the kind of r2 score you're looking for. Usually better to use r2 score on the log of the values, since otherwise it will be bias to over-penalize miss-prediction on very large numbers (where the error range is usually inherently bigger)
 
-accuracy = r2_score(np.log(real_val_arr), np.log(exact_prediction_arr_no_nan))
-print(f'Got an r2 score of the logs of the price: {accuracy}')
+accuracy = mean_absolute_error([x if i not in ignore_indexes_arr else 0  for i, x in enumerate(real_val_arr)], [x if i not in ignore_indexes_arr else 0 for i, x in enumerate(exact_prediction_arr)])
+print(f'Mean absolute error: {accuracy}')
 
 # Something better, let's see how many of our predictions fall within the predicted range
 correct = 0
 incorrect = 0
 for i, cr in enumerate(predicted_range_arr):
+    if i in ignore_indexes_arr:
+        continue
     if cr[0] < real_val_arr[i] < cr[1]:
         correct += 1
     else:
@@ -84,6 +84,8 @@ correct = 0
 incorrect = 0
 not_used = 0
 for i, cr in enumerate(predicted_range_arr):
+    if i in ignore_indexes_arr:
+        continue
     if prediction_confidence_arr[i] < high_percentile_confidence:
         not_used += 1
     elif cr[0] < real_val_arr[i] < cr[1]:
@@ -97,6 +99,8 @@ print(f'Out of all predictions {correct} of the intervals contained the actual v
 # Ok, let's try something useful. Maybe some of our wines are actually under-price and we could be selling them for more. Look for predictions where we have a very high confidence AND the lower bound of the interval is above the predicted value. Whilst not certain, this is a good indicator that a wine is overpriced (or maybe it's indeed cheap, but we are making a branding mistake by describing it as an "expensive" wine but selling it for cheap)
 
 for i, cr in enumerate(predicted_range_arr):
+    if i in ignore_indexes_arr:
+        continue
     confidence = prediction_confidence_arr[i]
-    if confidence > high_percentile_confidence and real_val_arr[i] < cr[0]:
-        print('The wine:\n' + str(test_data.iloc[i]) + f'\nIs potentially being sold at a price lower than it\'s real market value, we have a confidence of {confidence*100}% that this wine could be sold at a price between {cr[0]}$ and {cr[1]}$ but it\'s actual cost was {real_val_arr[i]} !')
+    if confidence >= high_percentile_confidence and real_val_arr[i] < cr[0] < cr[1]:
+        print('The wine:\n' + str(test_data.iloc[i]) + f'\nIs potentially being sold at a price lower than it\'s real market value, we have a confidence of {confidence*100}% that this wine could be sold at a price between {cr[0]}$ and {cr[1]}$ but it\'s actual cost was {real_val_arr[i]}$ !')
