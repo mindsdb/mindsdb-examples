af1752cc86bc77b948705eecc76ac266778d199f
George3d6 <george@cerebralab.com>
2020-07-15 19:44:44 +0300

    changed example

--- accuracy_determination_example/main.py
+++ accuracy_determination_example/main.py
@@ -1,8 +1,16 @@
 from sklearn.metrics import balanced_accuracy_score
 from mindsdb import Predictor
+import mindsdb
 
 
+# Method 1
 predictor = Predictor(name='heart_disease_predictor')
-validation_data = predictor.learn('target', 'heart.csv')
-accuracy = predictor.test(when_data=validation_data, accuracy_score_functions=balanced_accuracy_score)
+predictor.learn('target', 'heart.csv')
+accuracy = predictor.test(when_data=predictor.transaction.input_data.validation_df, accuracy_score_functions=balanced_accuracy_score)
 print(accuracy)
+
+# Method 2
+accuracy =  mindsdb.F.validate('target', 'heart.csv',balanced_accuracy_score)
+print(accuracy)
+
+# Method 3 (work in progress)
