997e16cc326682f20797182df0d9fb922ca68e7d
George3d6 <george@cerebralab.com>
2020-07-17 02:46:48 +0300

    comitting to showcase a bug

--- benchmarks/cifar_100/video_tutorial.py
+++ benchmarks/cifar_100/video_tutorial.py
@@ -1,6 +1,7 @@
 import mindsdb
 import csv
 from sklearn.metrics import accuracy_score
+import pandas as pd
 
 # Path to our train and test files
 train_file = 'train_sample.csv'
@@ -10,10 +11,10 @@ test_file = 'test_sample.csv'
 predictor = mindsdb.Predictor(name='cifrar_100_predictor')
 
 # Train it
-predictor.learn(from_data=train_file, to_predict=['class'], use_gpu=True, backend='lightwood', sample_margin_of_error=0.01, ignore_columns=['superclass'])
+predictor.learn(from_data=pd.read_csv(train_file).iloc[0:50], to_predict=['class'], ignore_columns=['superclass'])
 
 # Predict the class for each image on the testing set
-predictions = predictor.predict(when_data=test_file)
+predictions = predictor.predict(when_data=pd.read_csv(test_file).iloc[0:5])
 predicted_class = list(map(lambda x: x['class'], predictions))
 
 # Get the real class for each image
